35d60a24cb8ef5984235122d9caed395f67c47f7
roshan-cell <95081242+roshan-cell@users.noreply.github.com>
2023-04-30 16:52:51 +0530

    creating server

--- server/public/srcipt.js
+++ server/public/srcipt.js
@@ -3,8 +3,9 @@ button.addEventListener("click", () => {
   fetch("http://localhost:3000/create-checkout-session", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/javascript",
     },
+    // sent the item u want
     body: JSON.stringify({
       items: [
         { id: 1, quantity: 3 },
@@ -12,12 +13,14 @@ button.addEventListener("click", () => {
       ],
     }),
   })
+  // Redirect the user the output
     .then(res => {
       if (res.ok) return res.json()
       return res.json().then(json => Promise.reject(json))
     })
     .then(({ url }) => {
-      window.location = url
+      console.log(url)
+      // window.location = url
     })
     .catch(e => {
       console.error(e.error)
--- server/server.js
+++ server/server.js
@@ -4,14 +4,18 @@ const express = require("express")
 const app = express()
 
 app.use(express.json()) ;
+app.use(express.static("public"))
 
 const stripe = require("stripe")(process.env.STRIPE_KEY)
 
 const storeItems = new Map([
     [1 , {priceInRupee: 1000 , name: "Learn React"}] ,
     [2 , {priceInRupee: 1000 , name: "Learn Fluter"}] ,
-    [3, {priceInRupee: 1000 , name: "Learn DSA"}] 
 ])
+
+app.post("/create-checkout-session" , (req,res)=> {
+    res.json({ url : "Hi" })
+})
 
 app.listen(process.env.PORT , (req,res) => {
     console.log("App is listening" , process.env.PORT)
