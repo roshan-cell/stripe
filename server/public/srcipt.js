8995c13f23ae85b6a44a3ceb6b9ba10da25ef71a
roshan-cell <95081242+roshan-cell@users.noreply.github.com>
2023-05-05 00:52:36 +0530

    Changes

--- server/public/srcipt.js
+++ server/public/srcipt.js
@@ -14,6 +14,7 @@ button.addEventListener("click", () => {
     }),
   })
   // Redirect the user the output
+  // Redirect the user the output
     .then(res => {
       if (res.ok) return res.json()
       return res.json().then(json => Promise.reject(json))
